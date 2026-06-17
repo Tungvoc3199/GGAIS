518dbe30af39b7568972242778d9a6fd7d0fd301
Tungvoc3199 <anhduongnguyen12521@gmail.com>
2026-06-17 23:39:21 +0700

    Wrap app with premium dialog provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import { PremiumDialogProvider } from './components/ui/PremiumDialogProvider';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PremiumDialogProvider>
+      <App />
+    </PremiumDialogProvider>
   </StrictMode>,
 );
